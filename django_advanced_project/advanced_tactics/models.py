ed0ab8765d0070ad820814b1089e731f7a3ba7bc
Clay Shaw <Claydshaw@gmail.com>
2015-05-18 13:35:40 -0700

    updates to templates and models

--- django_advanced_project/advanced_tactics/models.py
+++ django_advanced_project/advanced_tactics/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your models here.
+class Hobby(models.Model):
+    name = models.CharField(max_length=128)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User)
+
+    # The additional attributes will include.
+    birthdate = models.DateField(blank=True)
+
+    HAIR_COLOR_CHOICES = (
+        ('BLACK', 'Black'),
+        ('BROWN', 'Brown'),
+        ('RED', 'Red'),
+        ('BLONDE', 'Blonde'),
+        ('SALTNPEPPER', 'Salt N Peppa'),
+        ('GREEN', 'Green'),
+    )
+
+    hair_color = models.CharField(max_length=128,
+                                    choices=HAIR_COLOR_CHOICES,
+                                    blank=True)
+
+    favorite_hobby = models.OneToOneField(Hobby)
+
+    created = models.DateTimeField(default=datetime.now())
+
+    def __unicode__(self):
+        return self.user.username
