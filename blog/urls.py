e006e7e7a200eb68444febd3b4de519f94515c75
Clay Shaw <Claydshaw@gmail.com>
2015-05-19 18:51:09 -0700

    setup password change files, take 2

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,11 @@ urlpatterns = [
     url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
     url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/home'}, name='logged_out'),
     url(r'^profile/$', 'blog.views.profile', name='profile'),
+
+    url(r'^password_reset/$', 'django.contrib.auth.views.password_reset', name='password_reset'),
+    url(r'^password_reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+    'django.contrib.auth.views.password_reset_confirm',
+    name='password_reset_confirm'),
+    url(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete', name='password_reset_complete'),
     ]
